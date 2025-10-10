a05b07c6028fd42a77bac20b63c17cb5dd981bb5
Tealands <167418832+Tealands@users.noreply.github.com>
2025-10-10 10:53:40 +0900

    EasyNovel

---
+++ EasyNovel/Novel1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>// すべての標準ライブラリをインクルード
+#include <string>// 文字列操作に必要
+#include <iostream>// 入出力に必要
+#include <fstream> // ファイル操作に必要
+
+using namespace std; // 言語の設定
+using ll = long long; // llをlong longの別名として定義
+
+int main() {
+    // ファイルを開く
+    int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+    string A = "スターリン", B = "ヒトラー", C = "ムッソリーニ";
+    cout << "彼の名前は" << A << "! 20世紀の独裁者" << endl;
+    return 0;
+}
