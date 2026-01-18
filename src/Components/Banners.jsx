6acecafb600e50acafabd3eaa82c964f35c658e1
Tealands <167418832+Tealands@users.noreply.github.com>
2026-01-18 20:55:25 +0900

    広告変更

--- src/Components/Banners.jsx
+++ src/Components/Banners.jsx
@@ -5,7 +5,7 @@ const Banners = () => {
     <div className="flex flex-col gap-4 w-full">
       <div className="w-full h-28 flex items-center justify-center">
         <a href="https://www.mod.go.jp/gsdf/jieikanbosyu/about/recruit/yobijieikanho.html" target="_blank"
-          className="flex items-center gap-3 bg-gradient-to-r from-blue-700 to-white text-white px-14 py-2 rounded-lg shadow-lg font-bold text-lg hover:from-blue-800 hover:to-gray-900 transition duration-300 ease-in-out w-full h-full">
+          className="flex items-center gap-3 bg-gradient-to-r from-pink-700 to-white text-white px-14 py-2 rounded-lg shadow-lg font-bold text-lg hover:from-red-800 hover:to-gray-900 transition duration-300 ease-in-out w-full h-full">
           <img src="src/assets/MOD.png" alt="" className="w-10 h-10 rounded-full border-2 border-white bg-white" />
           <span>
             予備自衛官補って<br></br>
@@ -15,11 +15,11 @@ const Banners = () => {
       </div>
 
       <div className="w-full h-28 flex items-center justify-center">
-        <a href="https://store.steampowered.com/app/394360/Hearts_of_Iron_IV/?l=japanese" target="_blank"
-          className="flex items-center gap-3 bg-gradient-to-r from-pink-700 to-white text-white px-14 py-2 rounded-lg shadow-lg font-bold text-lg hover:from-red-800 hover:to-gray-900 transition duration-300 ease-in-out w-full h-full">
-          <img src="src/assets/Hoi4.webp" alt="Hearts of Iron IV" className="w-12 h-10 rounded-full border-2 border-white bg-white" />
+        <a href="https://www.flightsimulator.com/" target="_blank"
+          className="flex items-center gap-3 bg-gradient-to-r from-blue-700 to-white text-white px-14 py-2 rounded-lg shadow-lg font-bold text-lg hover:from-blue-800 hover:to-gray-900 transition duration-300 ease-in-out w-full h-full">
+          <img src="src/assets/MSF.jpeg" alt="Microsoft Flight Simulator 2024" className="w-12 h-10 rounded-full border-2 border-white bg-white" />
           <span>
-           Hearts of Iron 4<br className="md:hidden" />
+           Microsoft Flight Simulator 2024<br className="md:hidden" />
            好評発売中！
           </span>
         </a>
